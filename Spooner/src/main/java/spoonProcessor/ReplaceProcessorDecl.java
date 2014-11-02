af0f0a828022bfe29847cbafd5261d943ec924bb
Exocen <kureijy@gmail.com>
2014-11-02 21:22:39 +0100

    test class

---
+++ Spooner/src/main/java/spoonProcessor/ReplaceProcessorDecl.java
@@ -0,0 +1,36 @@
+package spoonProcessor;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.declaration.CtVariable;
+import spoon.reflect.reference.CtTypeReference;
+
+import java.util.List;
+
+
+    public class ReplaceProcessorDecl extends AbstractProcessor<CtVariable> {
+        List<CtTypeReference<?>> actualTypeArguments;
+        CtTypeReference actualType;
+        public void process(CtVariable varDecl) {
+            actualTypeArguments = varDecl.getType().getActualTypeArguments();
+            actualType = varDecl.getType();
+            System.out.println(actualType);
+            if (actualTypeArguments.toString().equals("[java.lang.String]")) {// summum de la mocheté a changer impérativement
+
+               actualType.setSimpleName("LinkedList");
+              //  actualTypeArguments.get(0).setSimpleName("android.linge.truc");
+            }
+        }
+
+        @Override
+        public void processingDone() {
+            String simpleName, body;
+            char header;
+            super.processingDone();
+
+
+
+        }
+    }
+
+
